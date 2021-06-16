e8df6969bee480e513090bf3cc31514dee4f2ff5
Johnny Chen <johnnychen94@hotmail.com>
2021-06-16 23:58:28 +0800

    split kernel codes apart

--- src/strategies/full_search_cuda.jl
+++ src/strategies/full_search_cuda.jl
@@ -1,6 +1,14 @@
 using .CUDA
 
-function fullsearch_kernel!(R, ref, frame, f, rₚ, Δₛ, rₛ)
+@inline function evaluate_dist(f, ref, frame, px, py, qx, qy, rₚx, rₚy)
+    val = Distances.eval_start(f, ref, frame)
+    @inbounds for ox in -rₚx:rₚx, oy in -rₚy:rₚy
+        val = Distances.eval_reduce(f, val, Distances.eval_op(f, frame[px+ox, py+oy], ref[qx+ox, qy+oy]))
+    end
+    return Distances.eval_end(f, val)
+end
+
+function best_match_fullsearch_kernel!(R, ref, frame, f, rₚ, Δₛ, rₛ)
     M, N = size(frame)
     rₚx, rₚy = rₚ.I
     Δₛx, Δₛy = Δₛ.I
@@ -17,29 +25,22 @@ function fullsearch_kernel!(R, ref, frame, f, rₚ, Δₛ, rₛ)
         return nothing
     end
 
-    T = eltype(frame)
     # NOTE:
     # For better performance, loop along the row order. This could
     # produce results that different to CPU versions on constant fields.
     @inbounds for px in px_start:Δx:px_end, py in py_start:Δy:py_end
-        qx_start = max(rₚx+1, px - rₛx)
-        qy_start = max(rₚy+1, py - rₛy)
-        qx_end = min(px_end, px + rₛx)
-        qy_end = min(py_end, py + rₛy)
-
-        min_val, min_pos = T(Inf), CartesianIndex(qx_start, qy_start)
-        for qx in qx_start:Δₛx:qx_end, qy in qy_start:Δₛy:qy_end
-            val = Distances.eval_start(f, ref, frame)
-            for ox in -rₚx:rₚx, oy in -rₚy:rₚy
-                val = Distances.eval_reduce(f, val, Distances.eval_op(f, frame[px+ox, py+oy], ref[qx+ox, qy+oy]))
-            end
-            val = Distances.eval_end(f, val)
+        qx_range = max(rₚx+1, px - rₛx):Δₛx:min(px_end, px + rₛx)
+        qy_range = max(rₚy+1, py - rₛy):Δₛy:min(py_end, py + rₛy)
+        min_val, min_pos_x, min_pos_y = eltype(frame)(Inf), first(qx_range), first(qy_range)
+        @inbounds for qx in qx_range, qy in qy_range
+            val = evaluate_dist(f, ref, frame, px, py, qx, qy, rₚx, rₚy)
             if val < min_val
                 min_val = val
-                min_pos = CartesianIndex(qx, qy)
+                min_pos_x = qx
+                min_pos_y = qy
             end
         end
-        R[px, py] = min_pos
+        R[px, py] = CartesianIndex(min_pos_x, min_pos_y)
     end
     return nothing
 end
@@ -59,7 +60,7 @@ function best_match(
     blocks = ceil.(Int, size(ref)./threads)
 
     cu_matches = CuArray(fill(CartesianIndex(0, 0), size(frame)))
-    @cuda threads=threads blocks=blocks fullsearch_kernel!(cu_matches, ref, frame, S.f, rₚ, Δₛ, rₛ)
+    @cuda threads=threads blocks=blocks best_match_fullsearch_kernel!(cu_matches, ref, frame, S.f, rₚ, Δₛ, rₛ)
 
     R_frame = CartesianIndices(cu_matches)
     R_frame = first(R_frame)+rₚ:last(R_frame)-rₚ
